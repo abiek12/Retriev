8512ba6d3a7a395514c45146218431585673e598
Abhishek <abhishekkanichery@gmail.com>
2026-07-18 23:21:36 +0530

    feat: added pdf loader

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,9 +1,15 @@
 import { Hono } from 'hono'
+import { fileLoaderFactory } from './loader'
+import { FileTypes } from './utils/enums'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.get('/', async (c) => {
+  const loader = fileLoaderFactory.createFileLoader(FileTypes.PDF);
+  const filePath = `${import.meta.dir}/test.pdf`;
+
+  const fileContent = await loader.load(filePath);
+  return c.json(fileContent);
 })
 
 export default app
